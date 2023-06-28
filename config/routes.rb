1b5a4e8667b7dc64bc63f1841d1d277e2cd8f285
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2023-06-28 14:00:27 +0000

    Start
    git commit -m Start

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about' , as: 'home/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only:[:index,:show,:edit]
-  resources :books, only:[:new,:create,:index,:show,:edit,:destroy]
+  resources :users, only:[:index,:show,:edit,:update]
+  resources :books, only:[:create,:index,:show,:edit,:updata,:destroy]
 end
